5903a3d2ee8ceac26eaa08db11b5c92509914b1f
abdallah al sawaqi <buzz644@yahoo.com>
2026-02-27 00:05:36 +0000

    new

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'vendor.active' => \App\Http\Middleware\EnsureVendorIsActive::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
